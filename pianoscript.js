085a859380fb7547bf0d02d0c8c8fec0088c1571
rudytak <ondra4sedlacek@gmail.com>
2021-07-30 21:18:37 +0200

    .

--- pianoscript.js
+++ pianoscript.js
@@ -2,25 +2,25 @@ const notes = document.querySelectorAll(".key");
 
 // Setting key-notes pairs
 const keys = {
-  0: "C",
-  1: "Db",
-  2: "D",
-  3: "Eb",
-  4: "E",
-  5: "F",
-  6: "Gb",
-  7: "G",
-  8: "Ab",
-  9: "A",
-  10: "Bb",
-  11: "B"
+    0: "C",
+    1: "Db",
+    2: "D",
+    3: "Eb",
+    4: "E",
+    5: "F",
+    6: "Gb",
+    7: "G",
+    8: "Ab",
+    9: "A",
+    10: "Bb",
+    11: "B"
 };
 
 // Setting Click events for each key
 notes.forEach(note => {
-  note.addEventListener("click", () => {
+    note.addEventListener("click", () => {
 
-  });
+    });
 });
 
 // Keypress events
@@ -56,36 +56,36 @@ notes.forEach(note => {
 
 function showPianoNotes(pianonotes) {
 
-for( let i = 0; i < 12; i++ ) {
-    let key = document.querySelector(`[data-note="${keys[i]}"]`);
-    //console.log(key)
-    // key.classList.remove(".white-active");
-    // key.classList.remove(".black-active");
-    // console.log(key.classlist);
+    for (let i = 0; i < 12; i++) {
+        let key = document.querySelector(`[data-note="${keys[i]}"]`);
+        //console.log(key)
+        // key.classList.remove(".white-active");
+        // key.classList.remove(".black-active");
+        // console.log(key.classlist);
 
 
-    if (key.classList.contains("white")) {
-      key.classList.remove("white-active");
+        if (key.classList.contains("white")) {
+            key.classList.remove("white-active");
 
-    } else {
-      key.classList.remove("black-active");
+        } else {
+            key.classList.remove("black-active");
+        }
     }
-  }
-
-  
-  for( let i = 0; i < pianonotes.length; i++ ) {
-    let key = document.querySelector(`[data-note="${keys[pianonotes[i]]}"]`);
-    //console.log(key)
-    // key.classList.remove(".white-active");
-    // key.classList.remove(".black-active");
-    // console.log(key.classlist);
 
 
-    if (key.classList.contains("white")) {
-      key.classList.add("white-active");
+    for (let i = 0; i < pianonotes.length; i++) {
+        let key = document.querySelector(`[data-note="${keys[pianonotes[i]]}"]`);
+        //console.log(key)
+        // key.classList.remove(".white-active");
+        // key.classList.remove(".black-active");
+        // console.log(key.classlist);
 
-    } else {
-      key.classList.add("black-active");
+
+        if (key.classList.contains("white")) {
+            key.classList.add("white-active");
+
+        } else {
+            key.classList.add("black-active");
+        }
     }
-  }
 }
