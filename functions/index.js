c1cc61a16b580b2863128bb4857c75672fa23e4b
nturczan-calarts <nturczan@calarts.edu>
2021-08-06 13:26:25 -0700

    add all cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const admin = require("firebase-admin");
+
+exports.getRooms = functions.https.onRequest((request, response) => {
+  admin.database().ref("/rooms").once("value", (snap) => {
+    response.send(snap.val());
+  });
+});
+
+exports.addRoom = functions.https.onRequest((request, response) => {
+  admin.database().ref("/rooms").push().set({
+    ip_address: request.ip,
+    name: request.query.name,
+  });
+  response.send("Room Created!");
+});
+
+exports.delRoom = functions.https.onRequest((request, response) => {
+  const ref = admin.database().ref("/rooms");
+  const query = ref.orderByChild("ip_address").equalTo(request.ip);
+  query.once("value", function(snapshot) {
+    snapshot.forEach(function(child) {
+      child.ref.remove().then(function() {
+        response.send("Room Removed");
+      });
+    });
+  });
 });
